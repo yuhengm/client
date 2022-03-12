c77acd17b36b4873bd5d511e15526e1e523dc2b5
yxhniord <yxhniord@gmail.com>
2022-03-12 15:13:27 -0800

    add map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import Marker from "./Marker";
 
-function Map() {
-  return <div className="map">Map</div>;
-}
+const Map = (props) => {
+  const [center, setCenter] = useState({ lat: 49.15, lng: -122.8 });
+  const [zoom, setZoom] = useState(10);
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyC_MUZ2Hqhs1Q4YOjMu8nKN3-fyXHJFnG0" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <Marker lat={49.15} lng={-122.8} name="My Marker" color="blue" />
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
